34c321cc85718642ac895c9a3365beb8ea23f10c
Tomas Cohen Arazi <tomascohen@theke.io>
2022-02-08 11:08:46 -0300

    Issue #1: Move logo to the static assets dir
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ const pm_bundle_path = path.join(pm_file_path, pm_name);
  * If no static files need to be served, set static_relative_path = []
  * 
  */
-const static_relative_path = ['static_files', 'datepicker'];
+const static_relative_path = ['static'];
 
 var static_absolute_path = [];
 
